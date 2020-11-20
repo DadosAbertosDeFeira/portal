6655d490836be40118089fcd063baf5cc6198d1a
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-11-20 03:22:24 -0300

    Configuração inicial do projeto  (#17)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    React: 'writable',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+  },
+};
---
+++ src/pages/colabore.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <>
+      <h1>Página de Colaboração</h1>
+      <a href="/">
+        <h2>Voltar para Home</h2>
+      </a>
+    </>
+  );
+}
