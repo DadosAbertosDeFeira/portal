e5cf044dbd87c02cb6574984b8938d65d1adbed9
filipebsmaia <filipebsmaia@gmail.com>
2020-11-24 10:53:33 -0300

    feat: adicionado componente de SEO nas páginas

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -1,6 +1,9 @@
+import SEO from '../components/SEO';
+
 export default function About() {
   return (
     <>
+      <SEO title="Sobre" image="assets/dadosabertosdefeira.png" />
       <h1>Página sobre Dados Abertos de Feira</h1>
       <a href="/">
         <h2>Voltar para Home</h2>
