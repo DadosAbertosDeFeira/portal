17692c1a5b1a571751557b4122f9d00d9c84f917
matheusrocha89 <matheus.rocha89@gmail.com>
2020-12-12 21:41:42 +0000

    Just leaving one version of the button on the stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -6,18 +6,6 @@ export const ButtonTemplate = (args) => <Button {...args} />;
 ButtonTemplate.args = { children: 'Button' };
 ButtonTemplate.storyName = 'Default';
 
-export const Small = ButtonTemplate.bind({});
-Small.args = {
-  ...ButtonTemplate.args,
-  small: true,
-};
-
-export const Rounded = ButtonTemplate.bind({});
-Rounded.args = {
-  ...ButtonTemplate.args,
-  rounded: true,
-};
-
 export default {
   title: 'Components/Button',
   component: Button,
