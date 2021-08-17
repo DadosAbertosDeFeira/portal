9dca068b86161806049306d1c62f2a0a46de3ab6
Filipe Maia <filipebsmaia@gmail.com>
2021-08-17 11:01:22 -0300

    Corrige deploy na netlify

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 
-module.exports = withImages();
+module.exports = withPlugins([withImages], {
+  target: 'serverless',
+});
