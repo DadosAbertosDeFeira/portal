63c9e036358ae21e2f298916b1d831a58bad21b9
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2021-09-04 08:53:28 +0100

    Mudando a condição de inicialização do sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-if (process.env.NODE_ENV === 'development' && SENTRY_DSN) {
+if (process.env.NODE_ENV === 'development') {
   Sentry.init({
     dsn:
       SENTRY_DSN ||
