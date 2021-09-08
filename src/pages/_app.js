88b14e2e3633179dfcba732951431cd2ed940ffe
Filipe Maia <filipebsmaia@gmail.com>
2021-09-08 13:02:42 -0300

    Adiciona google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as gtag from '../lib/gtag';
 import '../styles/globals.scss';
+
+const handleRouteChange = (url) => {
+  gtag.pageview(url);
+};
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+  const { events } = useRouter();
+  useEffect(() => {
+    events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [events]);
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
