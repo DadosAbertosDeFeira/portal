68f4530bc0411ae664da4b7f0e84fd14db252e5e
Ana Paula Gomes <apgomes88@gmail.com>
2021-09-10 09:19:37 +0200

    Adiciona Vivian, Velame, Nataly e Bruno

--- src/utils/volunteers.js
+++ src/utils/volunteers.js
@@ -1,7 +1,7 @@
 export default [
   {
-    name: 'Ana Paula',
-    role: 'Desenvolvedora',
+    name: 'Ana Paula Gomes',
+    role: 'Faz-tudo',
     picture: 'https://avatars.githubusercontent.com/anapaulagomes',
     github: 'anapaulagomes',
     linkedin: 'anapaulagomess',
@@ -30,11 +30,12 @@ export default [
     linkedin: 'emerson-oliveira-89ab859',
   },
   {
-    name: 'Maria Clara',
-    role: 'Desenvolvedora',
-    picture: 'https://avatars.githubusercontent.com/olarclara',
-    github: 'olarclara',
-    linkedin: 'mariaclarasantana',
+    name: 'Vivian Macedo',
+    role: 'Analista de Dados',
+    picture:
+      'https://media-exp1.licdn.com/dms/image/C4D03AQE9q7zoT86HQw/profile-displayphoto-shrink_400_400/0/1524262173908?e=1636588800&v=beta&t=MWsa3TgcxKNBe09bMa3Mh_a90YRMG--QxY_0zKb7Xw8',
+    github: 'vihvs',
+    linkedin: 'vivianmacedo',
   },
   {
     name: 'Rafael Gomes',
@@ -43,6 +44,30 @@ export default [
     github: 'gomex',
     linkedin: 'rbgomes',
     twitter: 'gomex',
+  },
+  {
+    name: 'Rafael Velame',
+    role: 'Jornalista',
+    picture:
+      'https://media-exp1.licdn.com/dms/image/C4D03AQG8ytEVvsZ44g/profile-displayphoto-shrink_400_400/0/1519088249816?e=1636588800&v=beta&t=lneQV2nQp5ZcTNQbEfy7yYN1FuEKcZUb_Q4iwE8IvSw',
+    // twitter: 'rafaelvelame',
+    linkedin: 'rafael-velame-242b98104',
+  },
+  {
+    name: 'Bruno Morassuti',
+    role: 'Conselheiro Jurídico',
+    picture: 'https://avatars.githubusercontent.com/jedibruno',
+    github: 'jedibruno',
+    // twitter: '',
+    linkedin: 'bruno-schimitt-morassutti-7a0b5b180',
+  },
+  {
+    name: 'Nataly Costa',
+    role: 'Design',
+    picture: 'https://avatars.githubusercontent.com/natalycosta',
+    github: 'natalycosta',
+    // twitter: '',
+    linkedin: 'natalycosta',
   },
 ].sort((person1, person2) => {
   return person1.name.localeCompare(person2.name); // Automatically alphabetize
