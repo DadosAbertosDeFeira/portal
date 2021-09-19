73c7a0d4fb347474df53841269ae8c921a0d1ca0
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2021-09-19 20:38:58 +0100

    Adicionando primeira versão dos dados a serem exibidos na sessão projetos

---
+++ src/utils/projects.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    name: 'Maria Quitéria',
+    type: ['raspagem de dados', 'busca de dados'],
+    description:
+      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+    github: '',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do Projeto Maria Quitéria',
+    },
+  },
+  {
+    name: 'Bot do DOU',
+    type: ['bot', 'twitter'],
+    description:
+      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+    github: '',
+    logo: {
+      path: '/assets/icons/bot-dou.svg',
+      description: 'Logo do projeto Bot do DOU',
+    },
+  },
+  {
+    name: 'Empacotador',
+    type: ['publicação de dados'],
+    description:
+      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+    github: '',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do Projeto Empacotador',
+    },
+  },
+  {
+    name: 'Podcast Cidades Abertas',
+    type: ['podcast'],
+    description:
+      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+    github: '',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do Podcast Cidades Abertas',
+    },
+  },
+  // {
+  //   name: 'Empacotador',
+  //   type: ['publicação de dados'],
+  //   description:
+  //     'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+  //   github: '',
+  //   logo: {
+  //     path: '/assets/icons/maria-quiteria.svg',
+  //     description: 'Logo do Projeto Empacotador',
+  //   },
+  // },
+];
