7333c8bc16ef3c6a5da590891d4dda7f9a87d3f5
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2021-09-20 22:48:57 +0100

    Adicionando mais projetos

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -14,7 +14,7 @@ export default [
     name: 'Bot do DOU',
     type: ['bot', 'twitter'],
     description:
-      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+      'Já pensou se você pudesse ler uma thread no Twitter com os pontos mais importantes do Diário Oficial de Feira de Santana ao invés de ler um documento enorme? Temos um bot pra isso!',
     link: 'https://twitter.com/DiarioDeFeira',
     logo: {
       path: '/assets/icons/bot-dou.svg',
@@ -25,7 +25,7 @@ export default [
     name: 'Empacotador',
     type: ['publicação de dados'],
     description:
-      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+      'Colocamos os dados que coletamos em pacotes, assim fica mais fácil para que pesquisadores, programadores e jornalistas acessem seu conteúdo.',
     link: 'https://github.com/DadosAbertosDeFeira/empacotador',
     logo: {
       path: '/assets/icons/maria-quiteria.svg',
@@ -36,11 +36,55 @@ export default [
     name: 'Podcast Cidades Abertas',
     type: ['podcast'],
     description:
-      'Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.',
+      'A discussão sobre dados abertos e transparência dentro das cidades ainda está muito tímida. Nós queremos mudar isso.',
     link: 'https://anchor.fm/cidadesabertas/',
     logo: {
       path: '/assets/icons/maria-quiteria.svg',
       description: 'Logo do Podcast Cidades Abertas',
+    },
+  },
+  {
+    name: 'Pedidos',
+    type: ['pedidos de informação'],
+    description:
+      'Gerencie e compartilhe publicamente o status dos seus pedidos de informação',
+    link: 'https://github.com/DadosAbertosDeFeira/pedidos',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do projeto Pedidos',
+    },
+  },
+  {
+    name: 'Prometido',
+    type: ['controle social'],
+    description:
+      'Promessa é dívida. Vamos acompanhar o que foi prometido na campanha.',
+    link: 'https://github.com/DadosAbertosDeFeira/prometido',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do projeto Prometido',
+    },
+  },
+  {
+    name: 'tcm-ba',
+    type: ['raspagem de dados'],
+    description:
+      'Aqui você encontrará raspadores para o site do Tribunal de Contas dos Municípios da Bahia.',
+    link: 'https://github.com/DadosAbertosDeFeira/prometido',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do projeto tcm-ba',
+    },
+  },
+  {
+    name: 'Tomba',
+    type: ['processamento de linguagem natural', 'ciência de dados'],
+    description:
+      'Identifique endereços, bairros e outras localizações brasileiras automaticamente em um texto.',
+    link: 'https://github.com/DadosAbertosDeFeira/prometido',
+    logo: {
+      path: '/assets/icons/maria-quiteria.svg',
+      description: 'Logo do projeto tomba',
     },
   },
   // {
