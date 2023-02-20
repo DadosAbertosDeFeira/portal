c3e4f4dd85600337389fb3b2f2637518cd292637
alvaro <alvaro.neto@ilegra.com>
2023-02-20 16:00:56 -0300

    move importação de fontes para o _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,9 +8,11 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
 
