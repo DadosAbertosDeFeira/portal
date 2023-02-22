8c0eb833dda2a6c858d0a15d384cefdbff1e7747
alvaro <alvaro.neto@ilegra.com>
2023-02-21 22:19:15 -0300

    cria componentes de formulário reutilizavéis para o contactForm

---
+++ src/components/form/Textarea/Textarea.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useController } from 'react-hook-form';
+
+function Textarea({ name, label, control, rules }) {
+  const { field: input, fieldState: state } = useController({
+    name,
+    control,
+    rules,
+    defaultValue: '',
+  });
+
+  return (
+    <label htmlFor={name}>
+      <textarea
+        aria-label={label}
+        className="input-field"
+        placeholder={label}
+        aria-errormessage={state.error}
+        aria-invalid={state.invalid}
+        {...input}
+      />
+
+      {state.error && (
+        <span
+          key={state.error}
+          className="text-alert text-sm anima-input-error-message"
+        >
+          {state.error.message}
+        </span>
+      )}
+    </label>
+  );
+}
+
+Textarea.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  control: PropTypes.objectOf(PropTypes.any).isRequired,
+  rules: PropTypes.objectOf(PropTypes.any),
+};
+
+Textarea.defaultProps = {
+  rules: {},
+};
+
+export default Textarea;
