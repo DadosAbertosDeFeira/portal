4545df67c7c3f2729bbbc6fb7596c5ba98115552
Alvaro Guimarães <alvaro.neto@ilegra.com>
2023-03-17 10:50:55 -0300

    Adiciona um componente de Layout (#123)
    * Adiciona um Layout
    
    * substitui div por um react fragment
    
    * remove a importação não usada do Fragment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import SEO from '../components/SEO';
-import Header from '../components/Header';
 import Button from '../components/Button';
 import ContactForm from '../components/ContactForm';
 import HowContribute from '../components/HowContribute';
@@ -18,13 +16,6 @@ export default function Home() {
   const router = useRouter();
   return (
     <>
-      <SEO
-        title="Dados Abertos de Feira"
-        image="assets/dadosabertosdefeira.png"
-        shouldExcludeTitleSuffix
-      />
-      <Header />
-
       <section className="bg-no-repeat bg-bottom bg-cover bg-city-pattern flex flex-col tablet:flex-row">
         <div className="flex-grow flex justify-end tablet:items-start tablet:order-last">
           <img
