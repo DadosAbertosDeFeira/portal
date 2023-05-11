6fa3b761a02982ef74b99b9dcda5b8161a34c3fe
Alvaro Guimarães <alvarogfn@hotmail.com>
2023-05-11 15:37:42 -0300

    Cria componentes de formulário reutilizavéis (#124)
    * cria componentes de formulário reutilizavéis para o contactForm
    
    * Corrige o erro no aria-errormessage
    
    * Adiciona testes unitários para os Inputs
    
    * adiciona os novos componentes no storybook
    
    * corrigi issues da pull request

---
+++ src/utils/validations/validations.js
@@ -0,0 +1,9 @@
+import regexes from './regexes';
+
+const validations = new Map();
+
+validations.set('email', {
+  pattern: (v) => regexes.get('email').test(v) || 'Informe um email válido.',
+});
+
+export default validations;
