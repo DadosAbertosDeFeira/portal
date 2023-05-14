a84749eac15d7cad71b39aab26868b7194962030
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-05-14 16:41:04 -0300

    adiciona página 404 apartir da pr 77

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,11 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
