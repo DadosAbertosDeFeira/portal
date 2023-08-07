9e81a5494ba94b5ffcd95a4a706070472f605de1
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-08-07 13:22:00 -0300

    adiciona pagina de contribua

---
+++ src/pages/contribua.tsx
@@ -0,0 +1,59 @@
+import { Box } from "atoms/Box";
+import { Contact } from "organisms/Contact";
+import type { ContactSocialMedia } from "organisms/Contact/types";
+import { Contribute } from "organisms/Contribute/Contribute";
+import {
+  RiFacebookFill,
+  RiGithubFill,
+  RiInstagramFill,
+  RiTwitterFill,
+} from "react-icons/ri";
+
+import { HeaderLayout } from "@/layouts/HeaderLayout";
+import { SeoLayout } from "@/layouts/SeoLayout";
+
+import type { NextPageWithLayout } from "./_app";
+
+const Contribua: NextPageWithLayout = () => {
+  const socialMedias: ContactSocialMedia[] = [
+    {
+      icon: RiTwitterFill,
+      name: "Twitter",
+      url: "https://twitter.com/DadosDeFeira",
+    },
+    {
+      icon: RiInstagramFill,
+      name: "Instagram",
+      url: "https://www.instagram.com/dadosabertosdefeira/",
+    },
+    {
+      icon: RiGithubFill,
+      name: "GitHub",
+      url: "https://github.com/DadosAbertosDeFeira",
+    },
+    {
+      icon: RiFacebookFill,
+      name: "Facebook",
+      url: "https://www.facebook.com/dadosabertosdefeira",
+    },
+  ];
+
+  return (
+    <main className="my-10">
+      <Contribute />
+      <Box className="my-20 h-1 w-full bg-gray-2" />
+      <Contact socialMedias={socialMedias} />
+    </main>
+  );
+};
+
+Contribua.getLayout = function getLayout(page) {
+  return (
+    <SeoLayout title="Contribua" image="assets/dadosabertosdefeira.png" suffix>
+      <HeaderLayout>{page}</HeaderLayout>
+    </SeoLayout>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Contribua;
