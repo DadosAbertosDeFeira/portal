dddbd20760dea45640cb044bade77bcb6b4fc372
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-08-07 13:44:09 -0300

    ajustes no componente text

--- src/components/atoms/Text/Text.tsx
+++ src/components/atoms/Text/Text.tsx
@@ -5,10 +5,10 @@ import { twMerge } from "tailwind-merge";
 export type TextVariants = "h1" | "h2" | "h3" | "p" | "span";
 
 const variants: Record<TextVariants, string> = {
-  h1: "font-montserrat text-dark desktop:text-xl text-lg font-semibold",
+  h1: "font-montserrat text-dark text-lg font-extrabold",
   h2: "",
   h3: "",
-  p: "font-roboto",
+  p: "font-roboto leading-10",
   span: "font-roboto",
 };
 
@@ -20,7 +20,7 @@ export const Text = forwardRef<HTMLElement, TextProps>(function Text(
   { variant = "p", className, children, ...props },
   ref
 ) {
-  const classes = twMerge("tablet:text-base", variants[variant], className);
+  const classes = twMerge(variants[variant], className);
   return createElement(
     variant,
     { className: classes, ...props, ref },
