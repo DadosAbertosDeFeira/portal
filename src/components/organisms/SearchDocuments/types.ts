fac3566432faf36e06ac926e88407868102d437e
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-08-14 15:55:46 -0300

    Removi o componente SeachDocuments

--- src/components/organisms/SearchDocuments/types.ts
+++
@@ -1,7 +0,0 @@
-import type { FilterSearchProps } from "organisms/FilterSearch";
-import type { ComponentPropsWithoutRef, ReactElement } from "react";
-
-export type SearchDocumentsProps = ComponentPropsWithoutRef<"section"> & {
-  searchForm: ReactElement<FilterSearchProps>;
-  searchSuggestions?: string[];
-};
