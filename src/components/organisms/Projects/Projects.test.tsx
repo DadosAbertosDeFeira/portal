44d84f35a521d2de7b41aae3ccf57da8fba5addb
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-08-14 16:22:53 -0300

    ajustes no componente projects

--- src/components/organisms/Projects/Projects.test.tsx
+++ src/components/organisms/Projects/Projects.test.tsx
@@ -1,15 +1,30 @@
 import { render } from "@testing-library/react";
+import type { Project } from "@utils/projects";
 
 import { Projects } from "./Projects";
+import type { ProjectsProps } from "./types";
 
 describe("Projects", () => {
-  const makeSut = () => {
-    return render(<Projects />);
+  const projectsMock: Project[] = [
+    {
+      description:
+        "Tem a missão de libertar dados do município de Feira de Santana. Responsável pela raspagem e o armazenamento.",
+      link: "https://github.com/DadosAbertosDeFeira/maria-quiteria",
+      logo: {
+        path: "https://test.com/image.png",
+        description: "Logo do Projeto Maria Quitéria",
+      },
+      name: "Maria Quitéria",
+      type: ["raspagem de dados", "busca de dados"],
+    },
+  ];
+
+  const makeSut = ({ projects = projectsMock }: Partial<ProjectsProps>) => {
+    return render(<Projects projects={projects} />);
   };
 
-  // eslint-disable-next-line jest/expect-expect
   it("renders correctly", () => {
-    const { asFragment } = makeSut();
+    const { asFragment } = makeSut({});
 
     expect(asFragment).toMatchSnapshot();
   });
--- src/components/organisms/Projects/Projects.tsx
+++ src/components/organisms/Projects/Projects.tsx
@@ -1,51 +1,52 @@
 import IconProject from "@assets/icons/icon-projects.svg";
 import { Image } from "atoms/Image";
 import { Link } from "atoms/Link";
-import { Text } from "atoms/Text";
 import { TitleSection } from "molecules/TitleSection";
 import React from "react";
 import { BiLinkExternal } from "react-icons/bi";
 
 import type { ProjectsProps } from "./types";
 
-export const Projects = ({ projects = [] }: ProjectsProps) => {
+export const Projects = ({ projects }: ProjectsProps) => {
   return (
-    <TitleSection title="Projetos" src={IconProject} alt="">
-      <ul className="grid auto-rows-auto grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] items-center justify-items-center gap-8 p-5 tablet:grid-cols-[repeat(auto-fill,_minmax(450px,_1fr))]">
+    <TitleSection
+      className="p-2 lg:px-20"
+      title="Projetos"
+      src={IconProject}
+      alt=""
+    >
+      <ul className="grid auto-rows-auto grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] items-center justify-items-center gap-8 md:grid-cols-[repeat(auto-fill,_minmax(450px,_1fr))]">
         {projects.map(({ name, description, link, type, logo }) => (
           <li className="h-full w-full" key={name}>
-            <section className="relative flex h-full w-full flex-row gap-2 overflow-hidden rounded shadow-3">
-              <Image
-                className="hidden h-[140px] max-h-[140px] w-[140px] max-w-[140px] object-cover tablet:block"
-                src={logo.path}
-                alt={logo.description}
-              />
-              <div className="mt-2 flex h-full flex-col gap-2 p-2">
-                <Text variant="h1" className="text-base">
-                  {name}
-                </Text>
-                <Text className="overflow-hidden text-sm leading-tight">
-                  {description}
-                </Text>
-                <ul className="mb-2 flex grow flex-row flex-wrap items-end gap-1 text-xs">
-                  {type.map((item) => (
-                    <li
-                      className="shrink rounded bg-gray-50 p-1 capitalize text-gray-700"
-                      key={item}
-                    >
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-                <Link
-                  href={link}
-                  className="absolute right-1 top-1 flex flex-row flex-nowrap items-center gap-1 text-sm"
-                >
-                  Link
-                  <BiLinkExternal fill="#3793df" />
-                </Link>
-              </div>
-            </section>
+            <Link className="h-full w-full" href={link}>
+              <section className="relative flex h-full w-full flex-row gap-2 overflow-hidden rounded shadow-lg">
+                <Image
+                  className="hidden h-[140px] max-h-[140px] min-h-[140px] w-[140px] min-w-[140px] max-w-[140px] object-cover md:block"
+                  src={logo.path}
+                  alt={logo.description}
+                />
+                <div className="mt-2 flex h-full flex-col gap-2 p-2">
+                  <h3 className="pr-12 text-base">{name}</h3>
+                  <p className="overflow-hidden text-xs leading-tight">
+                    {description}
+                  </p>
+                  <ul className="mb-2 flex grow flex-row flex-wrap items-end gap-1 text-xs">
+                    {type.map((item) => (
+                      <li
+                        className="shrink rounded bg-gray-50 p-1 capitalize text-gray-400"
+                        key={item}
+                      >
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                  <p className="absolute right-1 top-1 flex flex-row flex-nowrap items-center gap-1 text-sm text-blue-400">
+                    Link
+                    <BiLinkExternal fill="#3793df" />
+                  </p>
+                </div>
+              </section>
+            </Link>
           </li>
         ))}
       </ul>
